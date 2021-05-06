53eabb6a69670ff182a5dc0ff521c9e7d9d397bc
0ear <63788066+0ear@users.noreply.github.com>
2021-05-06 16:53:35 +0800

    Delete 0128_new directory

--- 0128_new/若透明背景不設定為白色.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-im = Image.open('../img/sample02.jpg').convert('RGB')
-box = (240, 195, 760, 700)
-image_crop = im.crop(box)
-im3 = Image.new('RGBA', (800, 800), (125, 125, 125, 0))
-im3.paste(image_crop, (100, 50))
-im3.save('sample02new.png')
