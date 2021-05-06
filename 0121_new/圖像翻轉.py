af4a498d581ba9e2cd33ea93d8f7b54e5ba6e77e
0ear <63788066+0ear@users.noreply.github.com>
2021-05-06 16:53:19 +0800

    Delete 0121_new directory

--- 0121_new/圖像翻轉.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import  matplotlib.pyplot as plt
-import numpy as np
-image = Image.open(('../img/sample02.jpg'))
-image2 = np.flipud(image) #上下翻轉
-image3 = np.fliplr(image) #左右翻轉
-plt.imshow(image2)
-plt.axis('off')
-plt.show()
--- 0121_new/圖像顯示.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-import  matplotlib.pyplot as plt
-image = Image.open(('../img/sample01a.jpg'))
-plt.imshow(image)
-plt.axis('off')
-plt.show()
