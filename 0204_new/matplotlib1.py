c311b5662e664a82362939748b2a81069ca43c64
0ear <63788066+0ear@users.noreply.github.com>
2021-05-06 16:53:41 +0800

    Delete 0204_new directory

--- 0204_new/matplotlib1.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-image = plt.imread('../img/luffy.jpg')
-plt.axis('off')  #關圖片旁邊的座標
-plt.imshow(image)
-plt.show()
--- 0204_new/matplotlib3.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import cv2,os
-from skimage import io
-image = plt.imread('../img/tree.jpg')
-img2 = cv2.imread('../img/tree..jpg') #opencv通道為bgr其他的是rgb因此跑出來會有色彩差異
-plt.imshow(image)
-plt.imshow(img2)
-plt.axis('off')
-plt.show()
